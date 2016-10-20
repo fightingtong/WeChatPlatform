565faea12dffcf899941e50ce6ff49f204ca6dcc
tonghp <tonghp@tongbu.com>
2016-10-20 11:55:10 +0800

    获取微信用户信息

--- WeChatPlatform/Authorization/AuthBase.cs
+++ WeChatPlatform/Authorization/AuthBase.cs
@@ -29,5 +29,26 @@ namespace WeChatPlatform.Authorization
 
             return token;
         }
+
+        /// <summary>
+        /// 基础支持：获取用户信息。
+        /// </summary>
+        /// <param name="access_token"></param>
+        /// <param name="openid"></param>
+        /// <returns></returns>
+        public static BaseWeChatUserEntity GetBaseWeChatUserInfo(string access_token, string openid)
+        {
+            string url
+                = string.Format("https://api.weixin.qq.com/cgi-bin/user/info?access_token={0}&openid={1}&lang=zh_CN"
+                , access_token, openid);
+
+            string jsonContent = Encoding.UTF8.GetString(new WebClient().DownloadData(url));
+            if (string.IsNullOrEmpty(jsonContent))
+                return null;
+
+            var user = JsonHelper.DeserializeJsonToObject<BaseWeChatUserEntity>(jsonContent);
+
+            return user;
+        }
     }
 }
---
+++ WeChatPlatform/Entity/BaseWeChatUserEntity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChatPlatform.Entity
+{
+    public class BaseWeChatUserEntity
+    {
+        /// <summary>
+        /// 是否订阅此公众号，0-未关注，将取不到其余信息。
+        /// </summary>
+        public int subscribe;
+
+        public string openid;
+
+        public string nickname;
+
+        public string headimgurl;
+
+        public int sex;
+
+        public string language;
+
+        public string city;
+
+        public string province;
+
+        public string country;
+
+        /// <summary>
+        /// 关注时间。
+        /// </summary>
+        public string subscribe_time;
+
+        /// <summary>
+        /// 绑定开发平台UnionID.
+        /// </summary>
+        public string unionid;
+
+        /// <summary>
+        /// 对粉丝的备注。
+        /// </summary>
+        public string remark;
+
+        /// <summary>
+        /// 用户所在分组。
+        /// </summary>
+        public int groupid;
+    }
+}
